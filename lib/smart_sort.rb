89ab0776ae41747eaa0c57237d7131cda5b2d3dd
Rajesh Raikwar <rajesh.raikwar1211@gmail.com>
2017-12-24 20:48:56 +0530

    merge sort added

--- lib/smart_sort.rb
+++ lib/smart_sort.rb
@@ -1,13 +1,15 @@
 require "smart_sort/version"
 require 'smart_sort/quick_sort'
+require 'smart_sort/merge_sort'
 
 module SmartSort
   def self.sort(arr, sorting_name)
     if sorting_name == 'quick_sort'
-      QuickSort.new(arr).sort(0 , arr.size - 1)
-    elsif sorting_name == 'merge_sort'
-    else
       return QuickSort.new(arr).sort(0 , arr.size - 1)
+    elsif sorting_name == 'merge_sort'
+      return MergeSort.new(arr).sort(0 , arr.size - 1)
+    else
+      return QuickSort.new.sort(arr, 0, arr.size - 1);
     end
     return arr
   end
--- spec/smart_sort_spec.rb
+++ spec/smart_sort_spec.rb
@@ -5,12 +5,16 @@ describe SmartSort do
     expect(SmartSort::VERSION).to eql("1.0.0")
   end
 
-  describe 'quick_sort' do
-    it "tests quick sort method" do
-      arr = [19, 28, 9, 10, 6, 16, 8, 1, 4, 24, 10, 2, 8, 3, 9, 13, 4, 10, 19, 1, 8, 18, 1, 5, 10, 17, 6, 26, 6, 10]
-      sorted_arr = [1, 1, 1, 2, 3, 4, 4, 5, 6, 6, 6, 8, 8, 8, 9, 9, 10, 10, 10, 10, 10, 13, 16, 17, 18, 19, 19, 24, 26, 28]
-      sorted_via_algo = SmartSort.sort(arr,'quick_sort')
-      expect(sorted_via_algo).to eql(sorted_arr)
-    end
+  before(:each) do
+    @arr = [19, 28, 9, 10, 6, 16, 8, 1, 4, 24, 10, 2, 8, 3, 9, 13, 4, 10, 19, 1, 8, 18, 1, 5, 10, 17, 6, 26, 6, 10]
+    @sorted_arr = [1, 1, 1, 2, 3, 4, 4, 5, 6, 6, 6, 8, 8, 8, 9, 9, 10, 10, 10, 10, 10, 13, 16, 17, 18, 19, 19, 24, 26, 28]
+  end
+
+  describe '#quick_sort' do
+    it { expect(SmartSort.sort(@arr,'quick_sort')).to eql(@sorted_arr)}
+  end
+
+  describe '#merge_sort' do
+    it { expect(SmartSort.sort(@arr,'merge_sort')).to eql(@sorted_arr)}
   end
 end
